5887f4bb67e3290f2f236deeb768d07d09d7c567
Richard Yu <richard.yqn@gmail.com>
2022-09-28 20:51:15 -0400

    bug fix

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,10 +1,8 @@
-// NOTE! The steps in this file are basically identical to the ones you
-// performed in the SpeakHello.js file.
-
 (function (window) {
   var speakWord = "Good Bye";
-  var byeSpeaker = function speak(name) {
+  var byeSpeaker = {};
+  byeSpeaker.speak = function speak(name) {
     console.log(speakWord + " " + name);
-  }
+  };
   window.byeSpeaker = byeSpeaker;
   })(window);
--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,7 +1,8 @@
 (function (window) {
   var speakWord = "Hello";
-  var helloSpeaker = function speak(name) {
+  var helloSpeaker = {};
+  helloSpeaker.speak = function speak(name) {
     console.log(speakWord + " " + name);
-  }
+  };
   window.helloSpeaker = helloSpeaker;
   })(window);
